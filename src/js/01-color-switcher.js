a7bb026896245c022d6ab281260741e77fc98c98
Sokolova-LV <leyla-@ukr.net>
2023-04-25 11:21:43 +0300

    goit-js-hw-09
    homework

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+
+btnStart.addEventListener('click', onStart);
+btnStop.addEventListener('click', onStop);
+
+function onStart() {
+    timerId = setInterval(getRandomHexColor, 500);
+};
+
+function onStop() {
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
